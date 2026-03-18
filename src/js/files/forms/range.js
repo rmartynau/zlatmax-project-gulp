e4939cf244eb9391a0d4962072baddf44fed7001
Raman Martynau <rmartynau@gmail.com>
2026-03-18 19:08:47 +0300

    fixed range.js

--- src/js/files/forms/range.js
+++ src/js/files/forms/range.js
@@ -23,10 +23,84 @@ export function rangeInit() {
 					'max': [Number(toValue.dataset.rangeTo)]
 				}
 			});
+
 			item.noUiSlider.on('update', function (values, handle) {
 				fromValue.value = values[0];
 				toValue.value = values[1];
 			});
+			
+		    fromValue.addEventListener('keydown', function (e) {
+                var values = item.noUiSlider.get();
+                var value = Number(values[0]);
+                // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+                var steps = item.noUiSlider.steps();
+                // [down, up]
+                var step = steps[0];
+                var position;
+                // 40 is key down.// 38 is key up, // 13 is enter,
+                switch (e.which) {
+                    case 13:
+                    item.noUiSlider.setHandle([0], this.value);
+                    break;
+                    case 38:
+                    // Get step to go increase slider value (up)
+                    position = step[1];
+                    // false = no step is set
+                    if (position === false) {
+                        position = 1;
+                    }
+                    // null = edge of slider
+                    if (position !== null) {
+                        item.noUiSlider.setHandle([0], value + position);
+                    }
+                    break;
+                    case 40:
+                    position = step[0];
+                    if (position === false) {
+                        position = 1;
+                    }
+                    if (position !== null) {
+                        item.noUiSlider.setHandle([0], value - position);
+                    }
+                    break;
+                }
+            });
+			toValue.addEventListener('keydown', function (e) {
+                var values = item.noUiSlider.get();
+                var value = Number(values[1]);
+                // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+                var steps = item.noUiSlider.steps();
+                // [down, up]
+                var step = steps[1];
+                var position;
+                // 40 is key down.// 38 is key up, // 13 is enter,
+                switch (e.which) {
+                    case 13:
+                    item.noUiSlider.setHandle([1], this.value);
+                    break;
+                    case 38:
+                    // Get step to go increase slider value (up)
+                    position = step[1];
+                    // false = no step is set
+                    if (position === false) {
+                        position = 1;
+                    }
+                    // null = edge of slider
+                    if (position !== null) {
+                        item.noUiSlider.setHandle([1], value + position);
+                    }
+                    break;
+                    case 40:
+                    position = step[0];
+                    if (position === false) {
+                        position = 1;
+                    }
+                    if (position !== null) {
+                        item.noUiSlider.setHandle([1], value - position);
+                    }
+                    break;
+                }
+            });
 		});
 	}
 }
